88eba6d3a05d8b7679c74e1892a95799a2e3da8a
Maxim <jybo@mail.ru>
2023-07-02 20:36:43 +1000

    Create BinarySearch.java

---
+++ BinarySearch.java
@@ -0,0 +1,49 @@
+/* Given an array of integers nums which is sorted in ascending order, and an integer target, write a function
+ to search target in nums. If target exists, then return its index. Otherwise, return -1.
+You must write an algorithm with O(log n) runtime complexity.
+
+Example 1:
+
+Input: nums = [-1,0,3,5,9,12], target = 9
+Output: 4
+Explanation: 9 exists in nums and its index is 4
+Example 2:
+
+Input: nums = [-1,0,3,5,9,12], target = 2
+Output: -1
+Explanation: 2 does not exist in nums so return -1
+
+ */
+
+public class BinarySearch {
+    public static void main(String[] args) {
+        int[] nums = {-1, 0, 3, 5, 9, 12};
+        int target = 2;
+
+        System.out.println(method(nums, target));
+    }
+
+    public static int method(int[] nums, int target) {  // создаем метод
+        int start = 0;                                  // определяем начальную точку
+        int finish = nums.length - 1;                   // определяем конечную точку
+        while (start <= finish) {                       // запускаем цикл до какого момента будет действовать
+            int mid = (start + finish) / 2;             // определяем среднюю точку
+            if (nums[mid] == target) {                  // 1-е условие
+                return mid;
+            } else if (nums[mid] > target) {            // 2-е условие
+                finish = mid - 1;
+            } else {                                    // 3 -е условие
+                start = mid + 1;
+            }
+        }
+        return -1;
+    }
+}
+
+
+
+
+
+
+
+
